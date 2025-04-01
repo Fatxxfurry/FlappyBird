2f0081ccd50e75f0dbe5b59bc005750c8713e4ee
Hung <ares01202975777@gmail.com>
2025-04-01 23:56:40 +0700

    bai3

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int boardHeight = 640;
@@ -8,13 +9,15 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
     Image backgroundImg;
     Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
 
     // bird
     public class Bird {
-        int x=boardWidth/8;
-        int y=boardHeight/2;
-        int width=34;
-        int height=24;
+        int x = boardWidth / 8;
+        int y = boardHeight / 2;
+        int width = 34;
+        int height = 24;
         Image img;
 
         Bird(Image img) {
@@ -22,11 +25,32 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         }
     }
 
+    int pipeX = boardWidth;
+    int pipeY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+
+    public class Pipe {
+        int x = pipeX;
+        int y = pipeY;
+        int width = pipeWidth;
+        int height = pipeHeight;
+        boolean isPassed = false;
+        Image img;
+
+        Pipe(Image img) {
+            this.img = img;
+        }
+    }
+
     Bird bird;
+    ArrayList<Pipe> pipes;
     int velocityY = 0;
+    int velocityX = -4;
     int gravity = 1;
 
     Timer gameLoop;
+    Timer pipeLoop;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -36,12 +60,23 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
         setBackground(Color.BLUE);
         backgroundImg = new ImageIcon(getClass().getResource("/flappybirdbg.png")).getImage();
-
         birdImg = new ImageIcon(getClass().getResource("/flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("/toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("/bottompipe.png")).getImage();
 
         bird = new Bird(birdImg);
+        pipes = new ArrayList<Pipe>();
 
-        gameLoop = new Timer(1000 / 16, this);
+        pipeLoop = new Timer(2000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+
+        pipeLoop.start();
+
+        gameLoop = new Timer(1000 / 30, this);
         gameLoop.start();
     }
 
@@ -49,15 +84,45 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         velocityY += gravity;
         bird.y += velocityY;
         bird.y = Math.max(bird.y, 0);
-        bird.y = Math.min(bird.y, boardHeight-bird.height);
+        bird.y = Math.min(bird.y, boardHeight - bird.height);
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX;
+        }
     }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(backgroundImg, 0, 0, null);
-        g.drawImage(bird.img, bird.x, bird.y,bird.width, bird.height, null);
+        drawImage(g);
     }
 
+    public void drawImage(Graphics g) {
+        g.drawImage(backgroundImg, 0, 0, null);
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
+    }
+
+    void placePipes() {
+        // (0-1) * pipeHeight/2.
+        // 0 -> -128 (pipeHeight/4)
+        // 1 -> -128 - 256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
+        int randomPipeY = (int) (pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2));
+        int openingSpace = boardHeight / 4;
+
+        Pipe topPipe = new Pipe(topPipeImg);
+        topPipe.y = randomPipeY;
+        pipes.add(topPipe);
+
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
+        pipes.add(bottomPipe);
+    }
+
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
